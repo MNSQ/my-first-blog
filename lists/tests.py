833ae0d8575dadaecbdc04f99df65384597e4e86
MNSQ <stefan_m1998@yahoo.com>
2020-08-31 19:26:55 +0300

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page  
+from lists.models import Item
 from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
@@ -13,14 +14,38 @@ class HomePageTest(TestCase):
         request = HttpRequest()  
         response = self.client.get('/')
         html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')s
+        expected_html = render_to_string('home.html')
         self.assertTrue(html.startswith('<html>'))  
         self.assertIn('<title>MNSQ BLOG</title>', html)  
         self.assertTrue(html.strip().endswith('</html>'))
-        ​self.assertTemplateUsed(response, 'wrong.html') 
+        ​self.assertTemplateUsed(response,'wrong.html') 
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
 # Create your tests here.

ecefe7d5991a4b9b12a9abf2b24aff96e74478ef
MNSQ <stefan_m1998@yahoo.com>
2020-08-31 15:58:10 +0300

    Installed Selenium webdriver

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,6 @@
-from django.test import TestCase
 from selenium import webdriver
 
 browser = webdriver.Chrome()
 browser.get('http://127.0.0.1:8000/')
 
 assert 'Django' in browser.title
-# Create your tests here.

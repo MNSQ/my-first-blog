9d58d38b1d0457bb47820db1db429736c7bf57eb
MNSQ <stefan_m1998@yahoo.com>
2020-08-31 16:48:38 +0300

    Installed chromedriver

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
+driver= webdriver.Chrome()
 browser = webdriver.Chrome()
 browser.get('http://127.0.0.1:8000/')
 

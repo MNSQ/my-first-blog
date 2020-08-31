5125b3aa73b3d263eaab6a0056a38afbb597bd59
MNSQ <stefan_m1998@yahoo.com>
2020-08-31 17:17:06 +0300

    Basic view now returns minimal HTML

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home_page(request):
+    return HttpResponse('<html><title>MNSQ BLOG</title></html>')
 
 # Create your views here.
